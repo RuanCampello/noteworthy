0ccbb27e67a37a131aa82b19bdeaf10aafc6ad85
RuanCampello <ruancampello888@gmail.com>
2024-02-23 23:15:41 -0300

    feat: add single favourite note view and fix redirect url depending on user's page

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -6,7 +6,7 @@ import { stripHTMLTags } from '@/utils/html-to-string';
 import { HTMLContent } from '@tiptap/react';
 import { setCookie } from 'cookies-next';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 interface NoteProps {
@@ -15,25 +15,46 @@ interface NoteProps {
   text: HTMLContent;
   colour: ColourType;
   date: number;
-  href: 'notes' | 'favourites'
+  href: 'notes' | 'favourites';
 }
 
-export default function Note({ name, text, colour, date, id, href }: NoteProps) {
+export default function Note({
+  name,
+  text,
+  colour,
+  date,
+  id,
+  href,
+}: NoteProps) {
   const [hovered, setHovered] = useState(false);
   const [focused, setFocused] = useState(false);
 
-  const pathname = usePathname()
-  const currentId = pathname.replace('/notes/', '')
+  const pathname = usePathname();
+
+  const currentId = pathname.replace('/notes/', '');
   useEffect(() => {
     if (currentId === id) setFocused(true);
   }, [currentId, id, focused]);
 
-  const redirectUrl = pathname.includes('notes') ? id : `${href}/${id}`
+  function formatRedirectUrl(): string {
+    if (pathname.includes('notes')) return id;
+    if (pathname.includes('favourites/') && href === 'favourites') return id;
+    if (pathname.includes('favourites/')) return `/notes/${id}`;
+    else return `${href}/${id}`;
+  }
+  const redirectUrl = formatRedirectUrl();
+
   const backgroundColour = Colours[colour];
   const setOpenNote = (id: string) => setCookie('open_note', id);
   return (
     <Link
-      href={redirectUrl}
+      href={
+        //if the user is inside a favourite note and click on a non favourite note
+        // remove the favourite from redirect link and transform into a note link
+        href === 'favourites' && pathname.includes('favourites/')
+          ? redirectUrl.replace('favourites', '')
+          : redirectUrl
+      }
       onClick={() => setOpenNote(id)}
       className='rounded-sm lg:p-5 p-2 w-full first:mt-1 last:mb-1 focus:outline-none z-10 select-none'
       style={{
