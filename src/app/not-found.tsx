4d219c20d374ef5664577326027c49b832752b00
RuanCampello <ruancampello888@gmail.com>
2024-02-25 19:54:27 -0300

    style: not found style minor changes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -16,15 +16,15 @@ export default function NotFound() {
   const { href, text, redirectTitle } = getNavigationInfo(pathname || '');
   return (
     <div className='h-screen w-screen flex flex-col gap-8 items-center justify-center text-neutral-200 bg-black'>
-      <div className={`flex items-center text-9xl ${gothicOne.className}`}>
+      <div className={`flex gap-3 items-center text-9xl ${gothicOne.className}`}>
         <span>4</span>
-        <div className='bg-neutral-200 rounded-full flex items-center p-4'>
+        <div className='bg-neutral-200 rounded-full flex items-center p-4 mt-4'>
           <Image
             src={Logo}
-            width={85}
-            height={85}
+            width={68}
+            height={68}
             alt='Logo'
-            className='brightness-0 saturate-100 invert-[10%] sepia-[18%] saturate-[25%] hue-rotate-[314deg] brightness-[97%] contrast-100'
+            className='brightness-0 saturate-100 invert-[10%] sepia-[18%] hue-rotate-[314deg] contrast-100'
           />
         </div>
         <span>4</span>
