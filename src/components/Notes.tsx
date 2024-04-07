6e1d3263648080bdad1293c8d44f8bbea353541f
RuanCampello <ruancampello888@gmail.com>
2024-04-07 11:14:21 -0300

    style: placeholder when note's empyt

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -7,6 +7,7 @@ import { getFilteredNotes, getFilter } from '@/utils/format-notes';
 import NoteContextMenu from './NoteContextMenu';
 import { getAllUserOrdinaryNotes } from '@/data/note';
 import { auth } from '@/auth';
+import { ReactNode } from 'react';
 
 export default async function Notes() {
   const session = await auth();
@@ -43,13 +44,13 @@ export default async function Notes() {
       </div>
       <div className='flex flex-col gap-1 overflow-y-scroll scrollbar-thin scrollbar-track-black scrollbar-thumb-silver xl:max-h-[400px] lg:max-h-[300px] max-h-[230px] px-5 pb-1'>
         {filteredNotes.length === 0 && searchParam ? (
-          <div className='bg-midnight h-[100px] w-full rounded-sm lg:p-5 p-2 outline-2 outline-offset-2 outline-dashed outline-silver text-silver my-2 select-none'>
-            <h1 className='text-lg line-clamp-2 overflow-clip'>
+          <PlaceholderWrapper>
+            <h1>
               No note with such name as{' '}
               <span className='italic font-medium'>{searchParam}</span>
             </h1>
-          </div>
-        ) : (
+          </PlaceholderWrapper>
+        ) : filteredNotes.length > 0 ? (
           filteredNotes.map((note) => {
             const { id, title, colour, content, createdAt, lastUpdate } = note;
             return (
@@ -71,8 +72,23 @@ export default async function Notes() {
               </NoteContextMenu>
             );
           })
+        ) : (
+          <PlaceholderWrapper>
+            <div className='flex flex-col h-full justify-between'>
+              <h1 className='italic'>Darkness here, and nothing more.</h1>
+              <p className='h-fit'>Try to fill with some note</p>
+            </div>
+          </PlaceholderWrapper>
         )}
       </div>
+    </div>
+  );
+}
+
+function PlaceholderWrapper({ children }: { children: ReactNode }) {
+  return (
+    <div className='bg-midnight h-[100px] w-full rounded-sm lg:p-5 p-2 outline-2 outline-offset-2 outline-dashed outline-silver text-silver my-2 select-none'>
+      {children}
     </div>
   );
 }
