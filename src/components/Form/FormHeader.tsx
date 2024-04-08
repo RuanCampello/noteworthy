6706d111e1ee3ff71e59ad75f89a081d95ecfeab
RuanCampello <ruancampello888@gmail.com>
2024-04-08 11:50:28 -0300

    style: add form header typing animation

--- src/components/Form/FormHeader.tsx
+++ src/components/Form/FormHeader.tsx
@@ -1,17 +1,64 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client';
+
 import Image, { StaticImageData } from 'next/image';
 import { Lora } from 'next/font/google';
+import { motion, useAnimation } from 'framer-motion';
+import { useEffect, useRef } from 'react';
 
 interface FormHeaderProps {
   image: StaticImageData;
 }
 
-const lora = Lora({subsets: ['latin']})
+const lora = Lora({ subsets: ['latin'] });
+
+const defaultAnimation = {
+  hidden: {
+    opacity: 0.15,
+    y: 8,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.1,
+    },
+  },
+};
 
 export default function FormHeader({ image }: FormHeaderProps) {
+  const control = useAnimation();
+  const ref = useRef(null);
+  const word = 'Noteworthy';
+
+  useEffect(() => {
+    control.start('visible');
+  }, []);
+
   return (
     <div className='flex gap-1 items-center pb-16'>
       <Image alt='noteworthy logo' src={image} width={48} height={52} />
-      <h1 className={`${lora.className} text-4xl font-bold`}>Noteworthy</h1>
+      <motion.h1
+        className={`${lora.className} text-4xl font-bold block`}
+        ref={ref}
+        initial='hidden'
+        animate={control}
+        variants={{
+          visible: { transition: { staggerChildren: 0.2 } },
+          hidden: {},
+        }}
+        aria-hidden
+      >
+        {word.split('').map((c, i) => (
+          <motion.span
+            key={i}
+            className='inline-block'
+            variants={defaultAnimation}
+          >
+            {c}
+          </motion.span>
+        ))}
+      </motion.h1>
     </div>
   );
 }
