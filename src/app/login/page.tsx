1eaca1c5554d03f19f53a5d216ebd6cbe4e9abf2
RuanCampello <ruancampello888@gmail.com>
2024-04-28 13:22:38 -0300

    fix(nextjs): add NextJS suspense boundary

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,21 @@
 import LoginForm from '@/components/Auth/LoginForm';
+import { Loader2 } from 'lucide-react';
+import { Suspense } from 'react';
 
 export default async function LoginPage() {
   return (
     <div className='flex justify-center h-screen w-screen overflow-hidden items-center bg-black'>
-      <LoginForm />
+      <Suspense fallback={<Loading />}>
+        <LoginForm />
+      </Suspense>
+    </div>
+  );
+}
+
+function Loading() {
+  return (
+    <div className='w-screen h-screen flex items-center justify-center'>
+      <Loader2 size={20} />
     </div>
   );
 }
