553755097a51028cded5d2455629201f2de14796
RuanCampello <ruancampello888@gmail.com>
2024-04-29 08:45:02 -0300

    fix: add return

--- src/app/new-password/page.tsx
+++ src/app/new-password/page.tsx
@@ -3,9 +3,11 @@ import LoadingSuspense from '@/components/LoadingSuspense';
 import { Suspense } from 'react';
 
 export default async function NewPasswordPage() {
-  <Suspense fallback={<LoadingSuspense />}>
-    <main className='w-screen h-screen overflow-hidden flex items-center justify-center'>
-      <NewPasswordForm />
-    </main>
-  </Suspense>;
+  return (
+    <Suspense fallback={<LoadingSuspense />}>
+      <main className='w-screen h-screen overflow-hidden flex items-center justify-center'>
+        <NewPasswordForm />
+      </main>
+    </Suspense>
+  );
 }
