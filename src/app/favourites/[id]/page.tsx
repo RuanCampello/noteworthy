e230ade10badda41b50feb25cca3301b7e567183
RuanCampello <ruancampello888@gmail.com>
2024-06-16 11:40:55 -0300

    feat: add warning and prevent content of loading on private notes

--- src/app/favourites/[id]/page.tsx
+++ src/app/favourites/[id]/page.tsx
@@ -1,7 +1,8 @@
 import NotFound from '@/app/not-found';
+import NotVisibleWarning from '@/components/NotVisibleWarning';
 import NoteEditor from '@/components/Note/NoteEditor';
 import NoteHeader, { Owner } from '@/components/Note/NoteHeader';
-import { getNoteById } from '@/queries/note';
+import { currentUser, getNoteById } from '@/queries/note';
 import { getUserById } from '@/queries/user';
 
 type Props = { params: { id: string } };
@@ -9,9 +10,13 @@ type Props = { params: { id: string } };
 export default async function Favourite({ params }: Props) {
   const note = await getNoteById(params.id);
   if (!note) return <NotFound />;
-  const { content, title, createdAt, id, lastUpdate, userId } = note;
+  const { content, title, createdAt, id, lastUpdate, userId, isPublic } = note;
   const owner = await getUserById(userId);
   if (!owner) return;
+
+  const user = await currentUser();
+  const isNoteVisible = user?.id === owner.id || isPublic;
+  if (!isNoteVisible) return <NotVisibleWarning />;
 
   const ownerResumed: Owner = {
     name: owner.name,
--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -1,18 +1,25 @@
 import NotFound from '@/app/not-found';
+import NotVisibleWarning from '@/components/NotVisibleWarning';
 import NoteEditor from '@/components/Note/NoteEditor';
 import NoteHeader, { Owner } from '@/components/Note/NoteHeader';
-import { getNoteById } from '@/queries/note';
-import { getUserById } from '@/queries/user';
+import { currentUser, getNoteById } from '@/queries/note';
+import { getUserById } from '@/server/queries/user';
 
 type Props = { params: { id: string } };
 
 export default async function NotePage({ params }: Props) {
   const id = params.id;
+
   const note = await getNoteById(id);
   if (!note) return <NotFound />;
-  const { content, title, createdAt, userId, lastUpdate } = note;
-  const owner = await getUserById(userId);
+  const owner = await getUserById(note.userId);
   if (!owner) return <NotFound />;
+
+  const { content, title, createdAt, lastUpdate, isPublic } = note;
+
+  const user = await currentUser();
+  const isNoteVisible = user?.id === owner.id || isPublic;
+  if (!isNoteVisible) return <NotVisibleWarning />;
 
   const resumedOwner: Owner = {
     name: owner.name,
