d1d1661c7452e9d5b4389d3d782bf4cb368b2f88
RuanCampello <ruancampello888@gmail.com>
2024-07-20 13:23:59 -0300

    style: correct aligment

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from '@/components/Auth/RegisterForm';
 
 export default function RegisterPage() {
   return (
-    <div className='flex justify-center h-screen items-center'>
+    <div className='flex justify-center w-full h-screen items-center'>
       <RegisterForm />
     </div>
   );
