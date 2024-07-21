1a4a7494b6973fc4f00237bcc82c731b56b959ea
RuanCampello <ruancampello888@gmail.com>
2024-07-21 08:50:33 -0300

    feat(i18n): implement translation on sub sidebars and placeholder screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import Placeholder from '@/components/Placeholder';
 import Sidebar from '@/components/Sidebar';
 import { FileText } from 'lucide-react';
+import { getTranslations } from 'next-intl/server';
 
 export default async function Home() {
+  const t = await getTranslations('HomePlaceholder');
+
   return (
     <div className='flex w-full relative h-screen'>
       <Sidebar />
-      <Placeholder
-        paragraph='Choose a note from the list on the left to view its contents, or create
-        a new note to add to your collection.'
-        text='Select a note to view'
-      >
+      <Placeholder paragraph={t('description')} text={t('title')}>
         <FileText size={80} strokeWidth={1} />
       </Placeholder>
     </div>
