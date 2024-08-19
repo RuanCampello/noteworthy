92531c2195f46b5b3c0d304df54f9b87fa0451ba
RuanCampello <ruancampello888@gmail.com>
2024-08-19 08:40:26 -0300

    feat: add redirect and icons on note widget

--- src/components/Note/NoteWidget.tsx
+++ src/components/Note/NoteWidget.tsx
@@ -2,7 +2,8 @@ import { Note } from '@/types/database-types';
 import { Colours } from '@/utils/colours';
 import { stripHTMLTags } from '@/utils/format';
 import { formatDistanceToNow } from 'date-fns';
-import { NotebookText } from 'lucide-react';
+import { NotebookText, Package, Sparkles } from 'lucide-react';
+import Link from 'next/link';
 
 interface NoteWidgetProps extends Note {
   user: {
@@ -12,22 +13,34 @@ interface NoteWidgetProps extends Note {
 
 export default function NoteWidget({
   title,
+  id,
   content,
   colour,
   lastUpdate,
   user,
+  isArchived,
+  isFavourite,
 }: NoteWidgetProps) {
   content = stripHTMLTags(content);
   const date = formatDistanceToNow(lastUpdate, { addSuffix: true });
+  const redirectUrl =
+    formatRedirect({
+      is_arc: isArchived,
+      is_fav: isFavourite,
+    }) + id;
 
   return (
-    <article className='border border-midnight p-3 rounded-lg max-w-52 flex flex-col gap-3'>
+    <Link
+      href={redirectUrl}
+      role='article'
+      className='border border-midnight p-3 rounded-lg max-w-52 flex flex-col justify-between gap-3 select-none'
+    >
       <header className='flex items-center gap-2'>
         <div
           className='p-[3px] rounded-sm'
           style={{ backgroundColor: Colours[colour] }}
         >
-          <NotebookText className='w-4 h-4 text-midnight' strokeWidth={2.5} />
+          {<RenderIcon is_fav={isFavourite} is_arc={isArchived} />}
         </div>
         <h3 className='font-garamound font-medium text-lg truncate'>{title}</h3>
       </header>
@@ -36,6 +49,41 @@ export default function NoteWidget({
         <span className='text-silver truncate'>{user.name}</span>
         <span className='shrink-0'>{date}</span>
       </footer>
-    </article>
+    </Link>
   );
+}
+
+interface Props {
+  is_fav: boolean;
+  is_arc: boolean;
+}
+
+function RenderIcon({ is_fav, is_arc }: Props) {
+  const condition = (Number(is_arc) << 1) | Number(is_fav);
+  const props = {
+    className: 'w-4 h-4 text-midnight',
+    strokeWidth: 2.5,
+  };
+
+  switch (condition) {
+    case 2:
+      return <Package {...props} />;
+    case 1:
+      return <Sparkles {...props} />;
+    default:
+      return <NotebookText {...props} />;
+  }
+}
+
+function formatRedirect({ is_arc, is_fav }: Props) {
+  const condition = (Number(is_arc) << 1) | Number(is_fav);
+
+  switch (condition) {
+    case 2:
+      return `/archived/`;
+    case 1:
+      return `/favourites/`;
+    default:
+      return `/notes/`;
+  }
 }
