4f1d19582980f8437d7bd113a95826eca3570505
RuanCampello <ruancampello888@gmail.com>
2024-10-03 11:34:28 -0300

    build: remove unused dependencies

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Command as CommandPrimitive } from 'cmdk';
 import * as React from 'react';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
 import { X } from 'lucide-react';
--- src/lib/s3/s3.dev.ts
+++
@@ -1,5 +0,0 @@
-export async function getSignedUrlForObject(key: string, type: string) {
-  throw new Error(
-    'S3 operations are not available in the development environment.',
-  );
-}
