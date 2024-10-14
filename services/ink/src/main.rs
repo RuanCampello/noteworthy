0e03dd4e3af5e1c254d12efc8e666ed097093d6c
RuanCampello <ruancampello888@gmail.com>
2024-10-14 14:31:50 -0300

    build: actually correct dockerfile (never get first try )

--- services/ink/src/main.rs
+++ services/ink/src/main.rs
@@ -4,6 +4,6 @@ use ink::router;
 async fn main() {
   let app = router().await.expect("Failed to create router");
 
-  let listener = tokio::net::TcpListener::bind("0.0.0.0:6969").await.unwrap();
+  let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
   axum::serve(listener, app).await.unwrap();
 }
